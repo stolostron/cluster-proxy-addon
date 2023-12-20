0822868e73514554b482e4b8365adae4dd843d13
xuezhaojun <zxue@redhat.com>
2023-12-20 10:09:30 +0800

    In MCE2.5, remove the manifestwork that used to create cluster-proxy-service-proxy. (#134)
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- pkg/controllers/manager.go
+++ pkg/controllers/manager.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -10,12 +11,19 @@ import (
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterclient "open-cluster-management.io/api/client/cluster/clientset/versioned"
+	workclient "open-cluster-management.io/api/client/work/clientset/versioned"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 func init() {
@@ -64,6 +72,13 @@ func runControllerManager() error {
 	// Setup clients, informers and listers.
 	kubeConfig := config.GetConfigOrDie()
 
+	// In MCE 2.5, cluster-proxy-addon will delete the manifestwork that used to create the cluster-proxy-service-proxy
+	// TODO: remove this function in 2.6. @xuezhaojun
+	err := deleteClusterProxyServiceProxy(ctx, kubeConfig)
+	if err != nil {
+		return err
+	}
+
 	// secret client and lister
 	nativeClient, err := kubernetes.NewForConfig(kubeConfig)
 	if err != nil {
@@ -100,5 +115,43 @@ func runControllerManager() error {
 		klog.Error(err, "problem running manager")
 		return err
 	}
+	return nil
+}
+
+// In the previous version, cluster-proxy-addon create a manifestwork
+func deleteClusterProxyServiceProxy(ctx context.Context, kubeconfig *rest.Config) error {
+	clusterClient, err := clusterclient.NewForConfig(kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	workClient, err := workclient.NewForConfig(kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	managedclusters, err := clusterClient.ClusterV1().ManagedClusters().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, mc := range managedclusters.Items {
+		// get manifestwork, if the manifestwork is not found, do nothing
+		_, err := workClient.WorkV1().ManifestWorks(mc.Name).Get(ctx, "addon-cluster-proxy-service-proxy", metav1.GetOptions{})
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				klog.Infof("manifestwork addon-cluster-proxy-service-proxy not found in managedcluster %s", mc.Name)
+				continue
+			}
+			return fmt.Errorf("failed to get manifestwork addon-cluster-proxy-service-proxy in managedcluster %s: %v", mc.Name, err)
+		}
+
+		// delete manifestwork
+		err = workClient.WorkV1().ManifestWorks(mc.Name).Delete(ctx, "addon-cluster-proxy-service-proxy", metav1.DeleteOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to delete manifestwork addon-cluster-proxy-service-proxy in managedcluster %s: %v", mc.Name, err)
+		}
+	}
+
 	return nil
 }
