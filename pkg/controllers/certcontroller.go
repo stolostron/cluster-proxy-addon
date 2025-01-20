ecd823cd41802c4145d28d437c15b77c9ad34d8d
xuezhao <zxue@redhat.com>
2025-01-20 14:09:00 +0800

    Update hostnames to "*.open-cluster-management.proxy" (#236)
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- pkg/controllers/certcontroller.go
+++ pkg/controllers/certcontroller.go
@@ -62,7 +62,7 @@ func registerCertController(certNamespace string,
 				Namespace: certNamespace,
 				Name:      constant.ServerCertSecretName,
 				Validity:  time.Hour * 24 * 180, // align with the signer ca by cluster-proxy
-				HostNames: []string{"*", "localhost", "127.0.0.1", "*.proxy"},
+				HostNames: []string{"*", "localhost", "127.0.0.1", "*.open-cluster-management.proxy"},
 				Lister:    secertLister,
 				Client:    secertGetter,
 			},
