8c4706ef3a9ad3dcb0fa665404706e08e8960baa
xuezhaojun <zxue@redhat.com>
2026-02-02 22:56:49 +0800

    Add startup log for debugging purposes
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- cmd/cluster-proxy/main.go
+++ cmd/cluster-proxy/main.go
@@ -24,6 +24,7 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	klog.InitFlags(nil)
+	klog.V(2).Info("Starting cluster-proxy-addon application")
 	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
 	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 
